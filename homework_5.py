ee460d1281db1f567b5dc2745a5f7e8a93d09042
SolbDiez <167894154+SolbDiez@users.noreply.github.com>
2024-04-29 17:26:00 +0300

    Add files via upload

---
+++ homework_5.py
@@ -0,0 +1,16 @@
+# список
+my_list = ["яблоко", "апельсин", "мандарин", "ананас", "груша", "селедка"]
+print(my_list)
+# my_list_print = {0, -1}
+# for print_ in sorted(my_list_print, reverse=True) : print(my_list[print_])
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = "грейпфрукт"
+print(my_list)
+# словарь
+my_dict = {'apple': 'яблоко', 'orange': 'апельсин', 'mandarin': 'мандарин', 'pear': 'груша'}
+print(my_dict)
+print(my_dict['apple'])
+my_dict['herring'] = 'селедка'
+print(my_dict)
