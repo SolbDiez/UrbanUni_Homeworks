a0d625dc4c03e95e9f1f84814558000a849309d2
SolbDiez <167894154+SolbDiez@users.noreply.github.com>
2024-05-20 18:50:31 +0300

    Add files via upload

---
+++ homework_4_1_main.py
@@ -0,0 +1,8 @@
+from homework_4_1_module import print_3
+import homework_4_1_module as module
+
+print(dir(module))
+
+module.print_1()
+module.print_2()
+print_3()
---
+++ homework_4_1_module.py
@@ -0,0 +1,14 @@
+def print_1():
+    print('Hello world')
+
+def print_2():
+    print('I am from module 4.1')
+
+def print_3():
+    print('I am global from module 4.1')
+
+if __name__ == '__main__':
+    print_1()
+    print_2()
+    print_3()
+
